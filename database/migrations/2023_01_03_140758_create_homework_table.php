ca0f98f27a587e7c07cabcad4d5ddfece4bcb79a
themrb8 <officialroman786@gmail.com>
2023-01-03 20:54:44 +0600

    initial commit with class-1 practice

---
+++ database/migrations/2023_01_03_140758_create_homework_table.php
@@ -0,0 +1,41 @@
+curriculam_id<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('homework', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->text('link');
+            $table->unsignedBigInteger('curriculam_id')->default(0);
+            $table->unsignedBigInteger('exam_id')->default(0);
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('curriculam_id')->references('id')->on('curriculams')->onDelete('cascade');
+            $table->foreign('exam_id')->references('id')->on('exams')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('homework');
+    }
+};
